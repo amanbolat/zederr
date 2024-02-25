fd984411f81c7fe4979fe362788f37e408f59902
amanbolat <amanbolat@me.com>
2024-02-25 23:15:36 +0100

    refactor pkg packages

---
+++ pkg/net/fqdn.go
@@ -0,0 +1,27 @@
+package net
+
+import (
+	"unicode/utf8"
+
+	"golang.org/x/net/idna"
+)
+
+var idnaProfile = idna.New(
+	idna.ValidateLabels(true),
+	idna.VerifyDNSLength(true),
+	idna.StrictDomainName(true),
+)
+
+// FQDN reports whether is a valid fully qualified domain name.
+//
+// FQDN allows only ASCII characters as prescribed by RFC 1034 (A-Z, a-z, 0-9
+// and the hyphen).
+func FQDN(s string) bool {
+	for i := 0; i < len(s); i++ {
+		if s[i] >= utf8.RuneSelf {
+			return false
+		}
+	}
+	_, err := idnaProfile.ToASCII(s)
+	return err == nil
+}
